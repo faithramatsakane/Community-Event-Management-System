eafc96ad7964360049cdd5c9543d6b13511766d6
Pulane Makhotla <pulane.makhotla@dynamicdna.co.za>
2026-06-17 17:24:14 +0200

    Added Contact Us page

---
+++ js/contactus.js
@@ -0,0 +1,11 @@
+document
+  .getElementById("contactForm")
+  .addEventListener("submit", function (event) {
+
+    event.preventDefault();
+
+    alert("Message sent successfully!");
+
+    this.reset();
+
+});
